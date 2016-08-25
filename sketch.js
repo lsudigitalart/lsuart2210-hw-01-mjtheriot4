bf589e64e3c5cb2e5c141599f9d3ec8aa08415b8
mjtheriot4 <mther16@lsu.edu>
2016-08-25 11:34:23 -0500

    under construction

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+function setup(){
+  createCanvas(601, 601);
+
+  fill(50);
+  noStroke();
+  ellipse(300, 300, 600);
+
+  fill(100);
+  noStroke();
+  ellipse(340, 300, 500);
+
+  fill(150);
+  noStroke();
+  ellipse(370, 300, 400);
+
+  fill(200);
+  noStroke();
+  ellipse(400, 300, 270);
+
+}
